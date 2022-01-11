710917d82075ea21166181c52fc7cf3a56536746
rec.fffixing <imdex1009@gmail.com>
2022-01-12 02:36:09 +0900

    updated

---
+++ ch01/src/shortcut.java
@@ -0,0 +1,34 @@
+
+public class shortcut {
+	public static void main (String[] args) {
+		
+//		command shift L 단축키 전체 목록보기
+//		command +, - 폰트 크기 증가/ 감소
+//		command D 한줄 삭제
+//		command option down 행단위 복사
+//		command option A 멀티컬럼 편집
+//		option up/ down 행단위 이동
+//		command I 자동 들여쓰기
+//		command / 행 주석(토글)
+//		control space 자동완성 
+//		자동완성 템플릿을 커스텀 추가하고 싶다면?
+//		preference > JAVA > Editor > templete
+//		위 경로에서 임의적으로 템플릿을 추가하여 자동완성 기능을 확장할 수 있다  
+		
+		   
+		
+		System.out.println("Hello, world.");
+		System.out.println("Hello, world1.");
+		System.out.println("Hello, world.");
+		System.out.println("Hello, world.");
+		System.out.println("Hello, world.");
+		System.out.println("Hello, world.");
+		System.out.println("Hello, world.");
+		System.out.println("Hello, world.");
+		System.out.println("Hello, world.");
+		System.out.println("Hello, world.");
+		System.out.println("Hello, world.");
+		
+		
+	}
+}
