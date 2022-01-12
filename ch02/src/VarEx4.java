979673697a47663dbde58fcf35a068cdd8795487
rec.fffixing <imdex1009@gmail.com>
2022-01-12 16:55:19 +0900

    updated

--- ch02/src/VarEx4.java
+++ ch02/src/VarEx4.java
@@ -6,7 +6,24 @@ public class VarEx4 {
 		final int score; // = 100;
 		score = 200;
 		
-		System.out.println(score);
+		boolean power = true;
+		
+		byte b = 127; // -128 ~ 127
+		
+		int oct = 010; // 8진수, 10진수로 8
+		int hex = 0x10; // 16진수, 10진수로 16
+		
+		long l = 10_000_000_000L;
+		
+		float f = 3.14f; // float에서 f는 생략될 수 없
+		double d = 3.14; 
+		double d2 = 3.14f; // double에 f접미사를 사용한 값을 넣어도 괜찮
+		
+		System.out.println(10.);
+		System.out.println(.10);
+		System.out.println(10f);
+		System.out.println(1e3);
+		
 		
 
 	}
