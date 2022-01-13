3d972bcb57a477d7fa983ed35a64f3af5e2389dc
rec.fffixing <imdex1009@gmail.com>
2022-01-14 05:48:41 +0900

    updated

--- ch02/src/VarEx5.java
+++ ch02/src/VarEx5.java
@@ -4,8 +4,8 @@ public class VarEx5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Date d = new Date();
-		System.out.println(d);
+//		Date d = new Date();
+//		System.out.println(d);
 		
 		
 
--- ch02/src/printfEx1.java
+++ ch02/src/printfEx1.java
@@ -1,8 +1,33 @@
 
 public class printfEx1 {
 	public static void main(String[] args) {
-		System.out.println(10/3);
-//		정수 나누기 정수이기 때문에 결과 값이 3이나
-		System.out.println(10.0/3);
+//		System.out.println(10/3);
+////		정수 나누기 정수이기 때문에 결과 값이 3이나
+//		System.out.printf("%d%n", 15);
+//		System.out.printf("%o%n", 15);
+//		System.out.printf("%x%n", 15);
+//		System.out.printf("%s%n", Integer.toBinaryString(15));
+//		
+//		double f = 123.456789;
+//		System.out.printf("%f%n", f);
+//		System.out.printf("%e%n", f);
+//		System.out.printf("%g%n", f);
+//		
+//		System.out.printf("[%05d]%n", 10);
+//		
+//		
+		
+//		System.out.printf("[%s]%n", "hello"); // [hello]
+//		System.out.printf("[%8s]%n", "hello"); // [   hello]
+//		System.out.printf("[%-8s]%n", "hello"); // [hello   ]
+//		System.out.printf("[%.3s]%n", "hello"); // [hel]
+		
+		double d = 1.234567890;
+		System.out.printf("d=%14.10f%n", d);
+		
+		String str = "hello world";
+		System.out.print(str.length());
+				
+		
 	}
 }
