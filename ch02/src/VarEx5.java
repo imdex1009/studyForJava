3f53c52b5269756ff1768f4574426ff746570197
rec.fffixing <imdex1009@gmail.com>
2022-01-13 18:52:59 +0900

    updated

---
+++ ch02/src/VarEx5.java
@@ -0,0 +1,22 @@
+
+public class VarEx5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch = 'A';
+		int i = 'A';
+		
+		String str = "";
+		String str2 = "ABCD";
+		String str3 = "123";
+		String str4 = str2 + str3;
+		System.out.println(str4);
+		
+		System.out.println(ch);
+		System.out.println(i);
+		
+		
+
+	}
+
+}
