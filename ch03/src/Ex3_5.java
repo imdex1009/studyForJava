7cecf34d9e5a975061d4a8e4f119a9203cd349dd
rec.fffixing <imdex1009@gmail.com>
2022-01-23 15:40:30 +0900

    updated

---
+++ ch03/src/Ex3_5.java
@@ -0,0 +1,16 @@
+
+public class Ex3_5 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+    	double pi = 3.141592;
+    	double shortPi = Math.round(pi * 1000) / 1000.0;
+    	
+    	System.out.println(shortPi);
+    	System.out.println(Math.round(shortPi)/1.0);
+    	
+    	
+
+    }
+
+}
