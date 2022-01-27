2c9babc6ce83388adb165d99ce1d4d9a0403262b
rec.fffixing <imdex1009@gmail.com>
2022-01-27 11:37:52 +0900

    updated

---
+++ ch04/src/Ex4_8.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Ex4_8 {
+    public static void main(String[] args) {
+        int i = 5;
+
+        while (i-- != 0) {
+            System.out.println(i + " : hello!");
+        }
+    }
+}
