89842f569e3d3d0a6ecda21f233d0fe6c2d61032
rec.fffixing <imdex1009@gmail.com>
2022-01-30 11:26:07 +0900

    updated

---
+++ ch04/src/Ex4_10.java
@@ -0,0 +1,15 @@
+package src;
+
+
+import java.util.Scanner;
+
+public class Ex4_10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
