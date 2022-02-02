1933e684745c753633764878ebc21342227b73a8
rec.fffixing <imdex1009@gmail.com>
2022-02-02 18:50:41 +0900

    updated

---
+++ ch05/Ex5_2.java
@@ -0,0 +1,13 @@
+public class Ex5_2 {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        System.out.println("arr.length = "+arr.length);
+
+        for (int i=0; i<arr.length; i++) {
+            System.out.printf("arr[%d]=%d\n", i, arr[i]);
+        }
+
+
+
+    }
+}
