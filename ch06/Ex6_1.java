32783a3200a830f1204cd93681955f9ea10965ec
rec.fffixing <imdex1009@gmail.com>
2022-02-05 01:52:10 +0900

    updated

--- ch06/Ex6_1.java
+++ ch06/Ex6_1.java
@@ -1,15 +1,19 @@
 public class Ex6_1 {
     public static void main(String[] args) {
-
-
-
-
-
-
-    }
-
-
-    class Ex6_1_1 {
+        Tv t = new Tv();
+        t.channel = 7;
+        t.channelUp();
+        System.out.printf("현재 채널은 %d 입니다", t.channel);
 
     }
+}
+
+class Tv {
+    String color;
+    boolean power;
+    int channel;
+
+    void power() {power = !power;}
+    void channelUp() {channel++;}
+    void channelDown() {channel--;}
 }
