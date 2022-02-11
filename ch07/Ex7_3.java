abf116ebdd38a0c208f67a033515ff0671849ae2
rec.fffixing <imdex1009@gmail.com>
2022-02-11 16:25:15 +0900

    updated

--- ch07/Ex7_3.java
+++ ch07/Ex7_3.java
@@ -1,6 +1,12 @@
 class Point3D extends Point {
     int z;
 
+    Point3D(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
     String getLocation() {
         return "x:" + x + ", y:" + y + ", z:" + z;
     }
@@ -8,10 +14,7 @@ class Point3D extends Point {
 
 public class Ex7_3 {
     public static void main(String[] args) {
-        Point3D p = new Point3D();
-        p.x = 3;
-        p.y = 5;
-        p.z = 7;
+        Point3D p = new Point3D(3, 5, 7);
 
 
         System.out.println(p.getLocation());
---
+++ ch07/Ex7_4.java
@@ -0,0 +1,39 @@
+public class Ex7_4 {
+    public static void main(String[] args) {
+        Point3D_2 p3 = new Point3D_2(1, 2, 3);
+        System.out.println(p3.getLocation());
+    }
+}
+
+class Point_2 {
+    int x; int y;
+
+    Point_2(){
+        this(0,0);
+    }
+
+    Point_2 (int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
+
+class Point3D_2 extends Point_2{
+    int z;
+
+    Point3D_2() {
+        this(0,0,0);
+    }
+
+    Point3D_2(int x, int y, int z) {
+        super();
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    String getLocation() {
+        return "x:" + x + ", y:" + y + ", z:" + z;
+    }
+}
