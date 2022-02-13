cfca534f4d17d442a0763e7227ee0ae517d734a3
rec.fffixing <imdex1009@gmail.com>
2022-02-13 09:55:58 +0900

    updated

--- ch07/Ex7_7.java
+++ ch07/Ex7_7.java
@@ -1,6 +1,6 @@
 public class Ex7_7 {
     public static void main(String[] args) {
-        Car car = new Car();
+        Car car = null;
         FireEngine fe = new FireEngine();
         FireEngine fe2 = null;
 
