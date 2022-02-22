e576907487b2e9b4d548a163c5eda49847126aae
rec.fffixing <imdex1009@gmail.com>
2022-02-22 16:05:23 +0900

    updated

--- ch09/ch9_3.java
+++ ch09/ch9_3.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 class Card1 {
     String kind;
     int number;
@@ -10,6 +12,20 @@ class Card1 {
     Card1(String kind, int number) {
         this.kind = kind;
         this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Card1))
+            return false;
+        if (this == o) return true;
+        Card1 card1 = (Card1) o;
+        return number == card1.number && kind.equals(card1.kind);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(kind, number);
     }
 
     @Override
@@ -27,7 +43,13 @@ class ch9_3 {
         Card1 c1 = new Card1();
         Card1 c2 = new Card1();
 
+        System.out.println(c1.equals(c2));
+
         System.out.println(c1.toString());
         System.out.println(c2.toString());
+
+        System.out.println(c1.hashCode());
+        System.out.println(c2.hashCode());
+
     }
 }
