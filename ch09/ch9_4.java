23fb889f922c0dee148bd56a8a257982e5252a22
rec.fffixing <imdex1009@gmail.com>
2022-02-23 10:15:25 +0900

    updated

---
+++ ch09/ch9_4.java
@@ -0,0 +1,12 @@
+public class ch9_4 {
+    public static void main(String[] args) {
+        String s1 = "aaa";
+        String s2 = "aaa";
+        String s3 = "aaa";
+        String s4 = "bbb";
+
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
+
+    }
+}
