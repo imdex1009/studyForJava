f1f2b13d0faf761e2dd278c0e660c291ed277895
rec.fffixing <imdex1009@gmail.com>
2022-02-24 17:52:50 +0900

    updated

---
+++ ch09/ch9_5.java
@@ -0,0 +1,20 @@
+public class ch9_5 {
+    public static void main(String[] args) {
+
+        String s = new String("Hello");
+        System.out.println(s);
+
+        char[] c = {'H', 'e', 'l', 'l', 'o'};
+        String s1 = new String(c);
+        System.out.println(s1);
+
+        StringBuffer sb = new StringBuffer("Hello");
+        String s2 = new String(sb);
+        System.out.println(s2);
+
+
+
+
+    }
+
+}
