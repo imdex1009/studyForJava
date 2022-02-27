64626546323791e3d2d6d6ebf48cb5f041d4edb5
rec.fffixing <imdex1009@gmail.com>
2022-02-27 18:36:30 +0900

    updated

---
+++ ch09/Ch9_12.java
@@ -0,0 +1,11 @@
+
+
+public class Ch9_12 {
+    public static void main(String[] args) {
+        Integer i = 1;
+
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Long.MAX_VALUE);
+
+    }
+}
---
+++ ch09/Ch9_13.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Ch9_13 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(100);
+
+        Integer i = list.get(0);
+        System.out.println(i);
+    }
+}
