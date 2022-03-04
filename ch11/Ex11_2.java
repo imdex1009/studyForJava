bd1e0a3e2ef9dee7d21a0c63b5e4f93e768b8e9e
rec.fffixing <imdex1009@gmail.com>
2022-03-04 22:18:15 +0900

    updated

--- ch11/Ex11_2.java
+++ ch11/Ex11_2.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Ex11_2 {
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Ex11_2 {
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        Queue queue = new LinkedList();
+
+        stack.push("0");
+        stack.push("1");
+        stack.push("2");
+
+        queue.offer("0");
+        queue.offer("1");
+        queue.offer("2");
+
+        System.out.println("= Stack =");
+        while(!stack.empty()) {
+            System.out.println(stack.pop());
+        }
+
+        System.out.println("= Queue =");
+        while(!queue.isEmpty()) {
+            System.out.println(queue.poll());
+        }
+    }
 }
--- ch11/Ex11_3.java
+++ ch11/Ex11_3.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Ex11_3 {
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+public class Ex11_3 {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage:java Ecx11_3 \"EXPRESSION\"");
+            System.out.println("Example: java Ex11_3 \"(2+3)*3\"");
+            System.exit(0);
+        }
+
+
+        Stack stack = new Stack();
+        String EXPRESSION = args[0];
+
+        System.out.println("expression:" + EXPRESSION);
+
+        try {
+            for (int i = 0; i < EXPRESSION.length(); i++) {
+                char ch = EXPRESSION.charAt(i);
+
+                if (ch == '(') {
+                    stack.push(ch + "");
+                } else if (ch == ')') {
+                    stack.pop();
+                }
+            }
+
+            if (stack.isEmpty()) {
+                System.out.println("괄호가 일치합니다.");
+            } else {
+                System.out.println("괄호가 일치하지 않습니다.");
+            }
+        } catch (EmptyStackException e) {
+            System.out.println("괄호가 일치하지 않습니다.");
+        }
+    }
 }
