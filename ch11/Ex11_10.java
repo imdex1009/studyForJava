8d0db17d74045ae3c9135ca0f1a8fc79b18bb8b3
rec.fffixing <imdex1009@gmail.com>
2022-03-08 17:51:17 +0900

    updated

---
+++ ch11/Ex11_10.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Ex11_10 {
+    public static void main(String[] args) {
+        String[] fruits = new String[]{"Pineapple", "Banana", "Strawberry", "Grape", "Apple"};
+        System.out.println(Arrays.toString(fruits));
+
+        Arrays.sort(fruits);
+        System.out.println(Arrays.toString(fruits));
+
+
+    }
+}
