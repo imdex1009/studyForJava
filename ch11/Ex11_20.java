cdc6b411abb7ab09445802d14c7a1bca826c3d5e
rec.fffixing <imdex1009@gmail.com>
2022-03-11 00:58:30 +0900

    updated

--- ch11/Ex11_20.java
+++ ch11/Ex11_20.java
@@ -2,10 +2,38 @@ import java.util.*;
 
 class Ex11_20 {
     public static void main(String[] args) {
-        HashMap hm = new HashMap();
+        HashMap map = new HashMap();
+
+        map.put("myId", "1234");
+        map.put("asdf", "1111");
+        map.put("asdf", "1234");
+
+        Scanner s = new Scanner(System.in);
+
+        while(true) {
+            System.out.println("id와 password를 입력해주세요.");
+            System.out.println("id :");
+            String id = s.nextLine().trim();
+
+            System.out.print("password :");
+            String password = s.nextLine().trim();
+            System.out.println();
+
+            if(!map.containsKey(id)) {
+                System.out.println("입력하신 id는 존재하지 않습니다. 다시 입력해주세요.");
+                continue;
+            }
+
+            if(!(map.get(id).equals(password))) {
+                System.out.println("비밀번호가 일치하지 않습니다. 다시 입력해주세요.");
+            } else {
+                System.out.println("안녕하세요. " + id + "님 \n로그인이 완료되었습니다.");
+                break;
+            }
 
 
-        System.out.println(Objects.hash(1));
+        }
+
 
 
     }
