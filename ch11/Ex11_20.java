89791ce35f7ee91b907e7674c2ec53554a102103
rec.fffixing <imdex1009@gmail.com>
2022-03-11 00:26:01 +0900

    updated

---
+++ ch11/Ex11_20.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Ex11_20 {
+    public static void main(String[] args) {
+        HashMap hm = new HashMap();
+
+
+        System.out.println(Objects.hash(1));
+
+
+    }
+}
