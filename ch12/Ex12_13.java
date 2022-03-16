cd50f80e2c2adb6dc6f0a288d1185d09a7039e61
rec.fffixing <imdex1009@gmail.com>
2022-03-16 20:54:18 +0900

    updated

---
+++ ch12/Ex12_13.java
@@ -0,0 +1,16 @@
+class Parent {
+    void parentMethod() {}
+}
+
+class Child extends Parent {
+    @Override
+    void parentmethod() {
+    }
+}
+
+
+public class Ex12_13 {
+    public static void main(String[] args) {
+
+    }
+}
