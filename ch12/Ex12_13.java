1e16fbde24dc36a33baa677db71fd3f011be5f37
rec.fffixing <imdex1009@gmail.com>
2022-03-16 21:19:11 +0900

    updated

--- ch12/Ex12_13.java
+++ ch12/Ex12_13.java
@@ -2,15 +2,29 @@ class Parent {
     void parentMethod() {}
 }
 
-class Child extends Parent {
+class Child extends Parent implements Testable{
     @Override
-    void parentmethod() {
+    @Deprecated
+    void parentMethod() {
     }
+
+    @Override
+    public void test() {
+        System.out.println("hello");
+    }
+}
+
+@FunctionalInterface // 함수형 인터페이스는 하나의 추상 메서드만 가능
+interface Testable {
+    void test();
 }
 
 
 public class Ex12_13 {
+    @SuppressWarnings("deprecation") // 경고 억제
     public static void main(String[] args) {
-
+        Child c = new Child();
+        c.parentMethod(); // deprecated된 메서드 사용
+        c.test();
     }
 }
