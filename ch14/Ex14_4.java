57284e6a69d6a08a225ff40e727a4ae8960fc73e
rec.fffixing <imdex1009@gmail.com>
2022-03-26 13:21:24 +0900

    updated

--- ch14/Ex14_4.java
+++ ch14/Ex14_4.java
@@ -8,17 +8,20 @@ public class Ex14_4 {
         Consumer<Integer> c = i -> System.out.print(i+", ");
         Predicate<Integer> p = i -> i % 2 == 0;
         Function<Integer, Long> f = i -> (long)(i / 10 * 10);
+        UnaryOperator<Integer> u = i -> i * 2;
 
         List<Integer> list = new ArrayList<>();
         makeRandomList(s, list);
         System.out.println(list);
         printEvenNum(p, c, list);
         List<Long> newList = doSomething(f, list);
-        System.out.println(newList);
+        List<Integer> newList2 = doSomething(u, list);
+        System.out.println("newList1 " + newList);
+        System.out.println("newList2 " + newList2);
 
     }
 
-    static <T, R> List<R> doSomething(Function<T, R> f, List<T> list) {
+    private static <T, R> List<R> doSomething(Function<T, R> f, List<T> list) {
         List<R> newList = new ArrayList<>(list.size());
 
         for(T i : list) {
@@ -28,7 +31,7 @@ public class Ex14_4 {
         return newList;
     }
 
-    static <T> void printEvenNum(Predicate<T> p, Consumer<T> c, List<T> list) {
+    private static <T> void printEvenNum(Predicate<T> p, Consumer<T> c, List<T> list) {
         System.out.print("[");
         for(T i : list) {
             if(p.test((i)))
@@ -37,7 +40,7 @@ public class Ex14_4 {
         System.out.println("]");
     }
 
-    static <T> void makeRandomList(Supplier<T> s, List<T> list) {
+    private static <T> void makeRandomList(Supplier<T> s, List<T> list) {
         for(int i=0;  i < 10; i++) {
             list.add(s.get());
         }
